619155f9fb322c817193bc1c2f7fa9ec48a5bb8c
Tingting Chen <tingtingctt@gmail.com>
2020-04-30 21:25:43 -0700

    color coded timeblocks according to hour of the day

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // import * as moment from "moment";
 // var moment = require ('moment');
-var time = moment().format('dddd, MMMM Do YYYY');
-console.log(time);
-$("#currentDay").text(time);
+var date = moment().format('dddd, MMMM Do YYYY');
+var currentTime = moment().format('hh:mm a');
+$("#currentDay").append(date + "<br><br>" + currentTime);
+
+
+var currentHour = parseFloat(moment().format('HH'));
+console.log(currentHour);
+
+for (i=9; i<18; i++){
+    if (parseFloat($(".task").attr("id")) < currentHour) {
+        $("#"+i).addClass("past");
+    }
+    else if (parseFloat($(".task").attr("id")) = currentHour) {
+        $("#"+i).addClass("current");
+    }
+    else {
+        $("#"+i).addClass("future");
+    }
+}
